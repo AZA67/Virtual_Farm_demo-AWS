1f33ffb87def77f9572a44b214168983a96672d0
AZA <aza67@github.com>
2025-11-22 19:44:53 -0500

    omg

--- terraform/bootstrap/variables.tf
+++ terraform/bootstrap/variables.tf
@@ -1,11 +1,9 @@
 variable "project_name" {
   type = string
-  default = "attempt3000"
   description = "name of the project"
 }
 
 variable "region" {
-  default = "us-east-2"
   type = string
   description = "default region for the project"
 }
